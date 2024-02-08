f6b953bea320b26f97433de258b3cf7b1fc3df35
shrushtiimehta <82513385+shrushtiimehta@users.noreply.github.com>
2024-02-08 12:25:47 +0530

    Update vlog_read_script_sample.py

--- sample_scripts/vlog_read_script_sample.py
+++ sample_scripts/vlog_read_script_sample.py
@@ -1,5 +1,5 @@
 import torch
-
+import ast
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
 import os
@@ -27,11 +27,150 @@ from vlogger.videofusion import fusion
 from vlogger.videocaption import captioning
 from vlogger.videoaudio import make_audio, merge_video_audio, concatenate_videos
 from vlogger.STEB.model_transform import ip_scale_set, ip_transform_model, tca_transform_model
-from vlogger.planning_utils.gpt4_utils import (readscript, 
-                                               readtimescript, 
-                                               readprotagonistscript, 
-                                               readreferencescript, 
-                                               readzhscript)
+
+#story="In a bustling coffee shop, Alice, a young woman with blonde hair and a green apron, prepared to brew coffee. She approached the sleek, silver coffee machine, its buttons gleaming under the shop's warm lights. With practiced ease, she selected the finest beans and filled the grinder, the aroma filling the air. As the machine whirred to life, customers eagerly awaited their favorite brews. With each cup poured, the atmosphere buzzed with caffeine-fueled chatter and the aroma of freshly brewed coffee. Alice's skillful hands and the dependable machine transformed the shop into a haven for coffee lovers, one cup at a time."
+
+def ExtractProtagonist(story, file_path):
+    protagonists_places_dict = {}
+    protagonists_places_dict["Alice"] = "A young woman with blonde hair, wearing a green apron."
+    protagonists_places_dict["Coffee Shop"] = "Bustling with warm lights and a sleek, silver coffee machine."
+    return protagonists_places_dict
+
+def ExtractAProtagonist(story, file_path):
+    protagonists_places_dict = {}
+    protagonists_places_dict["Alice"] = "A young woman with blonde hair, wearing a green apron."
+    return protagonists_places_dict
+
+def protagonist_place_reference1(video_list, character_places, file_path):
+    new_video_list = []
+    num = 1
+    for video in video_list:
+        prompt = str(num) + ". " + video
+        new_video_list.append(prompt)
+        num += 1
+    key_list = []
+    i = 1
+    for key, value in character_places.items():
+        key_list.append(str(i) + ". " + key)
+        i += 1
+    reference_list = []
+    protagonists_places_reference = [{"video segment id": 1, "character/place id": [2]},{"video segment id": 2, "character/place id": [1]},{"video segment id": 3, "character/place id": [0]},{"video segment id": 4, "character/place id": [0]},{"video segment id": 5, "character/place id": [0]},
+                                 {"video segment id": 6, "character/place id": [0]},{"video segment id": 7,"character/place id": [1]},{"video segment id": 8,"character/place id": [0]},{"video segment id": 9,"character/place id": [0]},{"video segment id": 10,"character/place id": [0]},{"video segment id": 11,"character/place id": [1]},{"video segment id": 12,"character/place id": [0]},{"video segment id": 13,"character/place id": [0]}]
+    for key, value in character_places.items():
+        if key.lower() in prompt:
+          protagonists_places_reference[i]["character/place id"] = [1]
+    for protagonist_place_reference in protagonists_places_reference:
+        reference_list.append(protagonist_place_reference["character/place id"])
+    return reference_list
+
+def split_story(story, file_path):
+    video_list = ["Opening shot of a bustling coffee shop with warm lights and customers chatting. Alice, a young woman with blonde hair and a green apron, stands behind the sleek, silver coffee machine, preparing to brew coffee.", "Close-up of Alice's hands as she selects the finest coffee beans and fills the grinder. The aroma of coffee fills the air, blending with the background chatter of customers.",
+                  "Wide shot of the coffee machine as it whirs to life, steam rising from the spout. Customers eagerly await their orders, glancing at the menu board and chatting with baristas.","Over-the-shoulder shot of Alice as she expertly pours freshly brewed coffee into cups, her hands moving with precision. The aroma intensifies, enveloping the shop in a comforting scent.",
+                  "Cut to a montage of satisfied customers taking their first sips of coffee, their faces lighting up with delight. The atmosphere buzzes with energy and contentment.",  "Final shot of the coffee shop, with Alice smiling behind the counter as she serves another customer. The dependable coffee machine hums softly in the background, symbolizing the shop's role as a haven for coffee lovers."]
+    return video_list
+
+
+def patch_story_scripts(story, video_list, file_path):
+    video_list = ["Bustling coffee shop with Alice behind sleek coffee machine.","Close-up of Alice's hands filling grinder, aroma fills air.","Coffee machine whirs, steam rises, customers await orders.",
+    "Alice expertly pours coffee into cups, aroma intensifies.","Montage of satisfied customers taking first sips of coffee.","Final shot: Alice smiles, serves customer, coffee machine hums."]
+    return video_list
+
+
+def refine_story_scripts(video_list, file_path):
+    video_list = ["Busy coffee shop scene.","Close-up: Alice's hands.","Grinder fills with aroma.","Coffee machine whirs.","Steam rises from machine.","Customers wait for orders.",
+                  "Alice pours coffee.", "Aroma intensifies.","Customers take first sips.","Satisfied expressions.","Final shot: Alice smiles.","Customer served.","Coffee machine hums."]
+    return video_list
+
+
+def time_scripts(video_list, file_path):
+    try_times = 3
+    for i in range(try_times):
+        try:
+            new_video_list = []
+            num = 1
+            for video in video_list:
+                prompt = str(num) + ". " + video
+                new_video_list.append(prompt)
+                num += 1
+            time_scripts = [{"video fragment id": 1,"time": 3},{"video fragment id": 2,"time": 3},{"video fragment id": 3,"time": 3},{"video fragment id": 4,"time": 1},{"video fragment id": 5,"time": 1},{"video fragment id": 6,"time": 1},
+             {"video fragment id": 7,"time": 1},{"video fragment id": 8,"time": 1},{"video fragment id": 9,"time": 1},{"video fragment id": 10,"time": 1},{"video fragment id": 11,"time": 2},{"video fragment id": 12,"time": 1},{"video fragment id": 13,"time": 1}]
+            time_list = []
+            for time_script in time_scripts:
+                time = time_script["time"]
+                if time > 10:
+                    time = 10
+                time_list.append(time)
+            assert len(time_list) == len(video_list)
+            return time_list
+        except:
+            continue
+    assert len(time_list) == len(video_list)
+    return time_list
+
+def readscript(script_file_path):
+    with open(script_file_path, "r", encoding='utf-8') as f:
+        script = f.read()
+        video_fragments = ast.literal_eval(script)
+        video_list = []
+        for video_fragment in video_fragments:
+            video_list.append(video_fragment["video fragment description"])
+    return video_list
+
+
+def readzhscript(video_list,zh_file_path):
+    with open(zh_file_path, "r", encoding='utf-8') as f:
+        script = f.read()
+        video_list = video_list
+    return video_list
+
+
+def readtimescript(time_file_path):
+    with open(time_file_path, "r", encoding='utf-8') as f:
+        time_scripts = f.read()
+        time_scripts = ast.literal_eval(time_scripts)
+        time_list = []
+        for time_script in time_scripts:
+            frames = time_script["time"]
+            time_list.append(frames)
+    return time_list
+
+
+def readprotagonistscript(protagonist_file_path):
+    with open(protagonist_file_path, "r", encoding='utf-8') as f:
+        protagonist_scripts = f.read()
+        protagonist_scripts = ast.literal_eval(protagonist_scripts)
+        protagonists_places_dict = {}
+        for protagonist_script in protagonist_scripts:
+            protagonists_places_dict[protagonist_script["name"]] = protagonist_script["description"]
+    return protagonists_places_dict
+
+
+def readreferencescript(video_list, character_places, reference_file_path):
+    new_video_list = []
+    num = 1
+    for video in video_list:
+        prompt = str(num) + ". " + video
+        new_video_list.append(prompt)
+        num += 1
+    key_list = []
+    i = 1
+    for key, value in character_places.items():
+        key_list.append(str(i) + ". " + key)
+    with open(reference_file_path, "r", encoding='utf-8') as f:
+        reference_file = f.read()
+        reference_list = []
+        protagonists_places_reference = ast.literal_eval(reference_file)
+        for i, prompt in enumerate(video_list):
+            prompt = prompt.lower()
+            for j, key in enumerate(key_list):
+                if key.lower() in prompt:
+                    protagonists_places_reference[i]["character/place id"] = [j + 1]
+
+        for protagonist_place_reference in protagonists_places_reference:
+            reference_list.append(protagonist_place_reference["character/place id"])
+    return reference_list
+
+
 
 
 def auto_inpainting(args, 
@@ -279,7 +418,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, default="configs/vlog_read_script_sample.yaml")
+    parser.add_argument("--config", type=str, default="Vlogger/Vlogger/configs/vlog_read_script_sample.yaml")
     args = parser.parse_args()
     omega_conf = OmegaConf.load(args.config)
     save_path = omega_conf.save_path
